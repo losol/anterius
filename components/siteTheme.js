49687ab88519656101c8f360b60f4b07f40672d8
Ole Kristian Losvik <losvik@gmail.com>
2020-10-24 16:53:15 +0200

    Update CSSreset

--- components/siteTheme.js
+++ components/siteTheme.js
@@ -27,6 +27,18 @@ const SiteTheme = {
       700: "#2a69ac",
     },
   },
+  light: {
+    color: theme.colors.gray[700],
+    bg: theme.colors.red[500],
+    borderColor: theme.colors.gray[200],
+    placeholderColor: theme.colors.gray[500]
+  },
+  dark: {
+    color: theme.colors.whiteAlpha[900],
+    bg: theme.colors.gray[800],
+    borderColor: theme.colors.whiteAlpha[300],
+    placeholderColor: theme.colors.whiteAlpha[400]
+  },
   fontSizes: {
     xs: "0.75rem",
     sm: "0.875rem",
